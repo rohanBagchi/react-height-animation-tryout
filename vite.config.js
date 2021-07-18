deb2031d7d5ab19c86889b3bd2409862fc339fe8
Rohan Bagchi <rohan.bagchi01@gmail.com>
2021-07-18 12:51:45 +0530

    Revert "Updating rollup config to handle external files"
    This reverts commit 69fa7e97b6f09bb670fa57c362db2087625d71ce.

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  build: {
-    rollupOptions: {
-      external: 'github-ribbon.css'
-    }
-  }
+  plugins: [reactRefresh()]
 })
