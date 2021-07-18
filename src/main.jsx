8be2e839b0a3d8fe4153e6c672f5681c78acc3b5
Rohan Bagchi <rohan.bagchi01@gmail.com>
2021-07-18 12:53:52 +0530

    Moving gh ribbon inside src

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+import './index.css'
+import './github-ribbon.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <a
+      className="gh-ribbon darkblue right"
+      href="https://github.com/rohanBagchi/react-height-animation-tryout"
+      target="_blank"
+    >
+      View on GitHub!
+    </a>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
